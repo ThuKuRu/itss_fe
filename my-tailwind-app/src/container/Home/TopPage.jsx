76817ada0cbee226cfd094de0f7864726a349e85
Cao Thanh Tung <caothanhtung08082002@gmail.com>
2023-11-23 11:34:50 +0700

    this version is useful

--- my-tailwind-app/src/container/Home/TopPage.jsx
+++ my-tailwind-app/src/container/Home/TopPage.jsx
@@ -1,9 +1,22 @@
-import React, { useState } from "react";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import Sidebar from "../../layout/Sidebar/Sidebar";
 import Main from "../../layout/Main/Main";
-
+import { useContext } from "react";
+import { UserContext } from "../../UserContext";
+import { useNavigate } from "react-router-dom";
 const TopPage = () => {
   const [tab, setTab] = useState("upcoming");
+  const user = useContext(UserContext);
+  const navigate = useNavigate();
+  console.log(user);
+  useEffect(()=>{
+    if (user.jwt === "") {
+      navigate("/login");
+    }else{
+      console.log("co gi do sai!");
+    }
+  })
+
   return (
     <div className="flex gap-4">
       {/* <div className="w-1/5">sidebar</div> */}
