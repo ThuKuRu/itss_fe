41bd58aa982c87e2bea7f74d13bafe982dff1aee
Bui Trung Hung <hungsoi2002adc@gmail.com>
2023-12-05 21:20:42 +0700

    update thêm thông tin ban đầu

--- my-tailwind-app/src/container/Priority/Priority.jsx
+++ my-tailwind-app/src/container/Priority/Priority.jsx
@@ -3,6 +3,7 @@ import SearchBar from "./ComSearchBar/SearchBar";
 import 'react-responsive-modal/styles.css';
 import { Modal } from 'react-responsive-modal';
 import { Button, Form, Header } from "semantic-ui-react";
+import { FaPlus } from "react-icons/fa6";
 
 
 const Priority = () => {
@@ -13,7 +14,10 @@ const Priority = () => {
 
   const [open, setOpen] = useState(false);
   const [action,setAction] = useState('Add');
-  const [userdata, setUserdata] = useState([]);
+  const [userdata, setUserdata] = useState([
+    { name: 'Hùng', task: '5 ' },
+    { name: 'Tùng', task: '3 ' }
+  ]);
   const [user, setUser] = useState(blankuser);
   const [editIndex, setEditIndex] = useState(null);
 
@@ -25,9 +29,14 @@ const Priority = () => {
   }
 
   const addUser = () => {
-    setUserdata([...userdata,user]);
-    setUser(blankuser);
-    onCloseModal();
+    if (user.name && user.task) {
+      setUserdata([...userdata, user]);
+      setUser(blankuser);
+      onCloseModal();
+    } else {
+      // Hiển thị thông báo hoặc thực hiện các hành động khác nếu có lỗi
+      console.error('Vui lòng nhập đủ thông tin.');
+    }
   }
 
   const editUser = (index) => {
@@ -46,11 +55,23 @@ const Priority = () => {
       }
       return x
     });
+     
+    
     setUserdata(newusers);
     setUser(blankuser);
     setEditIndex(null);
     onCloseModal();
   }
+
+  const viewUser = (index) => {
+    console.log("index", index);
+    setAction('View');
+    const selectedUser = userdata.find((x, i) => i === index);
+    setUser(selectedUser);
+    setEditIndex(null);
+    onOpenModal();
+  };
+  
 
   const deleteUser = (index) => {
     const newusers = userdata.filter((x,i) => {return i !== index});
@@ -71,12 +92,13 @@ const Priority = () => {
               <div className='grow h-15'>
                 <span>
                 <h3>{user.name}</h3>
-                {user.task} task
+                {user.task} tasks
                 </span>
                 <hr className="mt-2" />
               </div>
               <div className='flex-none  h-15'>   
-                <Button className='ui button' color="red" onClick={() => editUser(index)}><span>Edit</span></Button>
+                <Button className='ui button' color="red" onClick={() => viewUser(index)}><span>View</span></Button>
+                <Button className='ui button' color="blue" onClick={() => editUser(index)}><span>Edit</span></Button>
                 <Button className='ui button' color="gray" onClick={() => deleteUser(index)}><span>Delete</span></Button>           
               </div> 
               
@@ -86,8 +108,14 @@ const Priority = () => {
           
       </div>
       
-      <div className="flex flex-col w-[20vw] h-fit  rounded pl-10 p-2">
-        <Button className='ui button' color="red" onClick={onOpenModal}><span>Add User</span></Button>
+      <div className="flex flex-col w-[15vw] h-fit  rounded pl-10 p-2">
+        <Button className='ui button' color="red" onClick={onOpenModal}>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+          <FaPlus className="text-sm font-light ml-2" />
+          <span style={{ marginLeft: '8px' }}>Add User</span>
+          </div>
+        </Button>
+
       </div>
       
 
@@ -110,7 +138,8 @@ const Priority = () => {
                 name="task"
                 value={user.task}
                 required
-                onChange={(e) => setUser({...user,"task":e.target.value})}
+                onChange={(e) => 
+                  setUser({...user, "task": e.target.value})}
             />
           
           {action === 'Add' && <Button className='ui button' color="red" onClick={() => addUser()}>Submit</Button>}
